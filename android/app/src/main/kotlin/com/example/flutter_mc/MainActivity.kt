37f1f146e1b32968c5f34636c445a5792fb119e3
luzhihong <xiaopikaqiu@outlook.com>
2024-09-11 10:51:14 +0800

    flutter create BasicMessageChannel instance

---
+++ android/app/src/main/kotlin/com/example/flutter_mc/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.flutter_mc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity(){
+
+
+
+
+
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  String? _message;
+
+  static const _channel = BasicMessageChannel('messageChannel', StringCodec());
+
+  @override
+  void initState() {
+    super.initState();
+
+    //Through the BasicMessageChannel instance, register a receive callback and return a message
+    _channel.setMessageHandler((message) async {
+      log('receive message:$message');
+      setState(() {
+        _message = message;
+      });
+
+      return "ACK from dart";
+    });
+  }
+
+  Future<void> _sendMessage() async {
+    //Send messages through the BasicMessageChannel instance
+    String? message = await _channel.send("Hello from dart");
+    log('send message:$message');
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
